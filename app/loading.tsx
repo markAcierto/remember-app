dc3ca19ac6ce8e80c183c61ac1f733f98286ffaf
Mark Acierto <123041575+markAcierto@users.noreply.github.com>
2026-08-26 13:01:33 -0400

    Delete app directory

--- app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center min-h-[60vh]">
-      <div className="w-8 h-8 rounded-full border-3 border-brand-pink/30 border-t-brand-pink animate-spin" />
-    </div>
-  );
-}
